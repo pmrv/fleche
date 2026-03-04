e7b6f6a4e62deccbc2edd0ec490c7dc1b8ee1e59
Marvin Poul <pmrv@posteo.de>
2026-03-03 23:36:03 -0500

    Improve Cache.calls typing
    make the broad argument init-only, and the field not in init

--- src/fleche/caches.py
+++ src/fleche/caches.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 import logging
-from dataclasses import dataclass, asdict, replace
+from dataclasses import dataclass, asdict, replace, field, InitVar
 from copy import copy
 from typing import Self, Iterable, Any, Union
 
@@ -121,11 +121,15 @@ class BaseCache(ABC):
 @dataclass
 class Cache(BaseCache):
     values: storage.Storage
-    calls: storage.CallStorage | storage.Storage
+    calls: storage.CallStorage = field(init=False)
 
-    def __post_init__(self):
-        if isinstance(self.calls, storage.Storage):
-            self.calls = storage.CallStorageAdapter(self.calls)
+    _calls: InitVar[storage.CallStorage | storage.Storage]
+
+    def __post_init__(self, _calls):
+        if isinstance(_calls, storage.Storage):
+            self.calls = storage.CallStorageAdapter(_calls)
+        else:
+            self.calls = _calls
 
         if not isinstance(self.values, storage.DestructuringStorage):
             self.values = storage.DestructuringStorage(self.values)
