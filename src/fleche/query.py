317ee420a2fd40df558dabb669087d75686776c3
Marvin Poul <pmrv@posteo.de>
2026-03-22 20:25:45 -0400

    Add arguments to query tables on request

--- src/fleche/query.py
+++ src/fleche/query.py
@@ -18,22 +18,40 @@ class QueryIterator(Iterable[call.LazyCall]):
     def __iter__(self) -> Iterator[call.LazyCall]:
         yield from self.calls
 
-    def table(self) -> pd.DataFrame:
+    def table(self, arguments: Iterable[str] = ()) -> pd.DataFrame:
         """Return a pandas DataFrame summarizing queried calls.
 
         Arguments and results are elided.
 
         The DataFrame index will be the lookup key (digest) of each call.
+        Columns are:
+            - `name`: the function name
+            - `module`: the module name
+            - metadata fields are flattened and added as columns directly
+
+        If given argument names collide with any of the above columns, the are prefixed by 'a_'.
+
+        Args:
+            arguments (iterable of str): add the given arguments (of the queried calls) as columns to the table
 
         Returns:
             :class:`pandas.DataFrame`: table of all calls on cache
         """
+
+        arguments = tuple(arguments)
 
         rows: dict[str, dict[str, Any]] = {}
         for c in self.calls:
             row = {
                     prop: getattr(c, prop) for prop in ("name", "module", "metadata")
             }
+            for a in arguments:
+                # TODO: quick and easy strategy to avoid name clashes, alternative would be to use multicolumns, but
+                # those are a bit annoying
+                if a not in row:
+                    row[a] = c.arguments.get(a, None)
+                else:
+                    row[f"a_{a}"] = c.arguments.get(a, None)
             md = row.pop("metadata", {}) or {}
             # Flatten each metadata name's dict into the row
             for data in md.values():
